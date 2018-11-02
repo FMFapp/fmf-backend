c1c178a81e2ad62e3906869d53a2b4fd6d5640c7
Itay Saadia <itay.1234567891000@gmail.com>
2018-11-02 15:33:59 +0200

    started the views branch and added the first stractur of the views

--- fmf_backend/person/views.py
+++ fmf_backend/person/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from person.models import Profile, User, Prescription
 
 # Create your views here.
+class UserController(View):
+    def get(self, request):
+        return
+
+    def post(self, request):
+        return
+
+    def delete(self, request):
+        return
+
+
+class ProfileController(View):
+    def get(self, request):
+        return
+
+    def post(self, request):
+        return
+
+    def delete(self, request):
+        return
+
+
+class PrescriptionController(View):
+    def get(self, request):
+        return
+
+    def post(self, request):
+        return
+
+    def delete(self, request):
+        return
+
