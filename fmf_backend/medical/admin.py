ed2a460601fe4f0c8b226adb4295e6640ab613b4
Itay Saadia <itay.1234567891000@gmail.com>
2018-11-02 18:21:12 +0200

    add admin panels and fixed some typoes

--- fmf_backend/medical/admin.py
+++ fmf_backend/medical/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Medication, Mutation, Seizure, Changes
 # Register your models here.
+admin.site.register(Medication)
+admin.site.register(Mutation)
+admin.site.register(Seizure)
+admin.site.register(Changes)
