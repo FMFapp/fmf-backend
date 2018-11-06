0a015bccf9fb60de6687796142295c36a27ded4a
Itay Saadia <itay.1234567891000@gmail.com>
2018-11-06 21:09:43 +0200

    bug fixes in the models according to the meeting with Dr. Giat

--- fmf_backend/person/models.py
+++ fmf_backend/person/models.py
@@ -9,7 +9,7 @@ TODO:
 
 from django.db import models
 from biological import models as bio
-from medical import models as med
+# import medical
 from facilities import models as fas
 import datetime
 
@@ -22,32 +22,37 @@ class Profile(models.Model):
             ("T", "meuhedet"),  # todo think about something
             )
     nikname = models.CharField(max_length=30)
-    birthday = models.DateField()
-    diagnosisDate = models.DateField()
-    originID = models.IntegerField()
+    birthdayYear = models.IntegerField()
+    diagnosisYear = models.IntegerField()
+    originID_fatherOfFather = models.IntegerField()
+    originID_fatherOfMother = models.IntegerField()
+    originID_motherOfFather= models.IntegerField()
+    originID_motherOfMother = models.IntegerField()
     seizureFrequency = models.IntegerField() # per year
     hmo = models.CharField(max_length=1, null=True)
+    # mutation = models.ManyToManyField(medical.models.Mutation)
 
     def __str__(self):
         return "Profile({_id}) name {name}".format(**{
              "_id": self.id if self.id else "Unknown",
-             "name": self.nickname if self.nickname else "Unknown",
+             "name": self.nikname if self.nikname else "Unknown",
              })
 
 
 class Prescription(models.Model):
-    medication = models.ForeignKey(med.Medication(), on_delete=models.CASCADE)
+    # time_taken = models.DateTimeField()
+    # medication = models.ForeignKey(medical.models.Medication, on_delete=models.CASCADE)
     frequency = models.IntegerField()
     amount = models.IntegerField()
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     isActive = models.BooleanField(default=True)
-    startDate = models.DateTimeField(default=datetime.datetime.fromtimestamp(2))  # time started to take the prespriction, NOT the creation of the instance
-    endDate = models.DateTimeField(default=datetime.datetime.fromtimestamp(1))  # todo fixme
+    startDate = models.DateField(default=datetime.datetime.fromtimestamp(2))  # time started to take the prespriction, NOT the creation of the instance
+    endDate = models.DateField(default=datetime.datetime.fromtimestamp(1))  # todo fixme
 
     def __str__(self):
         return "{isActive} Prescription({_id}): medication {med} frequency {freq} assinged to profile {prof}".format(**{
             "_id": self.id if self.id else "Unknown",
-            "med": self.medication.name if self.medication.name else "Unknown",
+            # "med": self.medication.name if self.medication.name else "Unknown",
             "freq": self.frequency if self.frequency else "Unknown",
             "prof": self.profile.nikname if self.profile.nikname else "Unknown",
             "isActive": self.isActive
@@ -58,13 +63,12 @@ class User(models.Model):
     pass
 
 
-class MdeicationUse(models.Model):
+class MedicationUse(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    time_taken = models.DateTimeField()
-    medication = models.ForeignKey(med.Medication, on_delete=models.CASCADE)
+    time_taken = models.DateTimeField(null=True)
+    # medication = models.ForeignKey(medical.models.Medication, on_delete=models.CASCADE)
     amount = models.IntegerField()
     place_taken = models.ForeignKey(fas.Facility, on_delete=models.CASCADE, null=True)
-
     def __str__(self):
         return f"profile {profile.nikname if profile.nikname else 'Unknown'} took {amount if amount else 'Unknown'} of {medication.name if medication.name else 'Unknown'} in {time_taken if time_taken else 'Unkwon'}"
 
